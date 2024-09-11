d55ed90a7bf80a4fa697e3d1b05044a62f5735fc
PixelGuy123 <89644121+PixelGuy123@users.noreply.github.com>
2024-09-11 14:41:31 -0300

    Support for multiple file drags

--- Program.cs
+++ Program.cs
@@ -24,52 +24,56 @@ static class BLDConverter
 				}
 			}
 		}
-		string file = args[0];
-
-		if (!ValidFile(file))
-		{
-			Console.BackgroundColor = ConsoleColor.Red;
-			Console.WriteLine($"Invalid file detected ({file}). Please input a .cbld file next time.");
-			Console.Read();
-			return;
-		}
-		file = Path.GetFullPath(file);
 		Console.Clear();
-		Stopwatch w = Stopwatch.StartNew();
-		try
+		for (int i = 0; i < args.Length; i++)
 		{
-			Console.WriteLine("Reading level..");
-			Level level;
-			using (var reader = new BinaryReader(File.OpenRead(file)))
-				level = LevelExtensions.ReadLevel(reader);
+			string file = args[i];
+			Console.WriteLine($"== Loading file: {Path.GetFileName(file)} ==\n");
 			
 
-			Console.WriteLine("Converting level...");
-			var dir = Path.GetDirectoryName(file);
-
-			if (string.IsNullOrEmpty(dir))
-				throw new DirectoryNotFoundException("Directory for the provided path has not been found.");
-
-			string fname = Path.Combine(dir, Path.GetFileNameWithoutExtension(file) + ".bld");
-
-			using (var writer = new BinaryWriter(File.OpenWrite(fname)))
-				level.ConvertToEditor().SaveIntoStream(writer);
+			if (!ValidFile(file))
+			{
+				LogAtColor(ConsoleColor.Red, $"Invalid file detected ({file}). Please input a .cbld file next time.");
+				continue;
+			}
+			file = Path.GetFullPath(file);
 			
+			Stopwatch w = Stopwatch.StartNew();
+			try
+			{
+				Console.WriteLine("Reading level..");
+				Level level;
+				using (var reader = new BinaryReader(File.OpenRead(file)))
+					level = LevelExtensions.ReadLevel(reader);
 
-			w.Stop();
-			LogAtColor(ConsoleColor.Green, $"CBLD file converted as {Path.GetFileName(fname)}");
-			Console.WriteLine("Time taken: " + w.ElapsedMilliseconds + "ms");
-			Console.WriteLine("Press any key to quit...");
-			Console.Read();
+
+				Console.WriteLine("Converting level...");
+				var dir = Path.GetDirectoryName(file);
+
+				if (string.IsNullOrEmpty(dir))
+					throw new DirectoryNotFoundException("Directory for the provided path has not been found.");
+
+				string fname = Path.Combine(dir, Path.GetFileNameWithoutExtension(file) + ".bld");
+
+				using (var writer = new BinaryWriter(File.OpenWrite(fname)))
+					level.ConvertToEditor().SaveIntoStream(writer);
+
+
+				w.Stop();
+				LogAtColor(ConsoleColor.Green, $"CBLD file converted as {Path.GetFileName(fname)}");
+				Console.WriteLine("Time taken: " + w.ElapsedMilliseconds + "ms");
+			}
+			catch (Exception e)
+			{
+				Console.BackgroundColor = ConsoleColor.Red;
+				Console.WriteLine($"Failed to load file ({file}). Please, make sure the file you\'re using is not corrupted or contain invalid data.");
+				Console.WriteLine($"Printing exception...\n{e}");
+				return;
+			}
 		}
-		catch (Exception e)
-		{
-			Console.BackgroundColor = ConsoleColor.Red;
-			Console.WriteLine($"Failed to load file ({file}). Please, make sure the file you\'re using is not corrupted or contain invalid data.");
-			Console.WriteLine($"Printing exception...\n{e}");
-			Console.Read();
-			return;
-		}
+
+		Console.WriteLine("====\nPress any key to quit...");
+		Console.Read();
 	}
 
 	static bool ValidFile(string path) =>
